82fb988c5891169fc1eb3417a800c82b50f775cb
bostanimfl <73405981+bostanimfl@users.noreply.github.com>
2025-07-26 09:35:30 +0330

    feat: Implement About screen
    This commit introduces the "About" screen to the application.
    
    Key changes:
    - Created `AboutScreen.kt` with a composable function `AboutContent` that displays the application's description and a back button.
    - Added string resources for the "About This App" title and a detailed description of the app's purpose and disclaimer in both English and Farsi.
    - Updated `Color.kt` to slightly change `backgroundLight` color.
    - Included a `Preview` for the `AboutContent` composable.

--- app/src/main/java/com/example/unitest/AboutScreen.kt
+++ app/src/main/java/com/example/unitest/AboutScreen.kt
@@ -1,12 +1,70 @@
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.unitest.R
 
 
 @Composable
 fun AboutScreen(navController: NavController) {
-    // Replace with your actual about screen content
-    Text(stringResource(R.string.about_page_description))
+    AboutContent { navController.navigateUp() }
+}
+
+@Composable
+fun AboutContent(onBackPressed: () -> Unit) {
+    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+        Surface(Modifier.padding(12.dp), shadowElevation = 4.dp, shape = ShapeDefaults.Medium) {
+            Column(Modifier.padding(top = 24.dp, bottom = 4.dp)) {
+                Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxWidth()) {
+                    Text(
+                        stringResource(R.string.about_this_app),
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                }
+                Text(
+                    stringResource(R.string.about_page_description),
+                    textAlign = TextAlign.Justify,
+                    modifier = Modifier.padding(
+                        top = 16.dp,
+                        bottom = 8.dp,
+                        start = 16.dp,
+                        end = 12.dp
+                    )
+                )
+                UButton(
+                    Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(horizontal = 12.dp)
+                        .fillMaxWidth(),
+                    label = stringResource(R.string.back)
+                ) {
+                    onBackPressed()
+                }
+            }
+        }
+
+    }
+
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun AboutScreenPreview() {
+    AboutContent { }
 }
